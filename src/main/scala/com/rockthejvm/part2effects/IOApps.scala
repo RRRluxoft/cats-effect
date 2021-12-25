41f467e7e4092b70d2ec2acf50695c9af18f2b36
Roman Lytvyshko <roman.lytvyshko@ocado.com>
2021-12-25 21:31:42 +0100

    IOApps

--- src/main/scala/com/rockthejvm/part2effects/IOApps.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOApps.scala
@@ -1,35 +1,36 @@
 package com.rockthejvm.part2effects
 
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.unsafe.IORuntime
 
 import scala.io.StdIn
 
 object IOApps {
   val program = for {
     line <- IO(StdIn.readLine())
-    _ <- IO(println(s"You've just written: $line"))
+    _    <- IO(println(s"You've just written: $line"))
   } yield ()
 }
 
 object TestApp {
-  import IOApps._
+  import IOApps.*
 
   def main(args: Array[String]): Unit = {
-    import cats.effect.unsafe.implicits.global
+//    import cats.effect.unsafe.implicits.global
+    given IORuntime = cats.effect.unsafe.implicits.global
     program.unsafeRunSync()
   }
 }
 
 object FirstCEApp extends IOApp {
-  import IOApps._
+  import IOApps.*
 
-  override def run(args: List[String]) =
+  override def run(args: List[String]): IO[ExitCode] =
+//    program.map(_ => ExitCode.Success)
     program.as(ExitCode.Success)
 }
 
 object MySimpleApp extends IOApp.Simple {
-  import IOApps._
-
-  override def run = program
+  import IOApps.program
+  override def run: IO[Unit] = program
 }
-
