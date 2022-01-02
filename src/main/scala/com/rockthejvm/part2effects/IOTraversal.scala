d092e9f3df4221c7f6a841f3c3510c2626f9dc07
Roman Lytvyshko <roman.lytvyshko@ocado.com>
2022-01-02 02:37:58 +0100

    IOTraversal

--- src/main/scala/com/rockthejvm/part2effects/IOTraversal.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOTraversal.scala
@@ -2,12 +2,13 @@ package com.rockthejvm.part2effects
 
 import cats.effect.{IO, IOApp}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
 object IOTraversal extends IOApp.Simple {
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  //  import scala.concurrent.ExecutionContext.Implicits.global
+  given ExecutionContext = ExecutionContext.global
 
   def heavyComputation(string: String): Future[Int] = Future {
     Thread.sleep(Random.nextInt(1000))
@@ -23,7 +24,7 @@ object IOTraversal extends IOApp.Simple {
   }
 
   import cats.Traverse
-  import cats.instances.list._
+  import cats.instances.list.*
   val listTraverse = Traverse[List]
 
   def traverseFutures(): Unit = {
@@ -33,7 +34,14 @@ object IOTraversal extends IOApp.Simple {
     singleFuture.foreach(println)
   }
 
-  import com.rockthejvm.utils._
+  import cats.Applicative
+  import cats.syntax.traverse.toTraverseOps
+  def traverseFutures_v2[T[_]: Traverse, G[_]: Applicative, A, B](fa: T[A])(f: A => G[B]) = {
+    val sf: G[T[B]] = fa.traverse(f)
+    IO.delay(sf)
+  }
+
+  import com.rockthejvm.utils.*
 
   // traverse for IO
   def computeAsIO(string: String): IO[Int] = IO {
@@ -43,37 +51,45 @@ object IOTraversal extends IOApp.Simple {
 
   val ios: List[IO[Int]] = workLoad.map(computeAsIO)
   val singleIO: IO[List[Int]] = listTraverse.traverse(workLoad)(computeAsIO)
+  def singleIO_v3(using listTraverse: Traverse[List]): IO[List[Int]] = listTraverse.traverse(workLoad)(computeAsIO)
 
   // parallel traversal
-  import cats.syntax.parallel._ // parTraverse extension method
+  import cats.syntax.parallel.* // parTraverse extension method
   val parallelSingleIO: IO[List[Int]] = workLoad.parTraverse(computeAsIO)
 
   /**
-   * Exercises
-   */
+    * Exercises
+    */
   // hint: use Traverse API
   def sequence[A](listOfIOs: List[IO[A]]): IO[List[A]] =
-    listTraverse.traverse(listOfIOs)(x => x)
+    Traverse[List].traverse(listOfIOs)(identity)
 
   // hard version
-  def sequence_v2[F[_] : Traverse, A](wrapperOfIOs: F[IO[A]]): IO[F[A]] =
-    Traverse[F].traverse(wrapperOfIOs)(x => x)
+  def sequence_v2[F[_]: Traverse, A](wrapperOfIOs: F[IO[A]]): IO[F[A]] =
+    Traverse[F].traverse(wrapperOfIOs)(identity)
 
   // parallel version
   def parSequence[A](listOfIOs: List[IO[A]]): IO[List[A]] =
-    listOfIOs.parTraverse(x => x)
+    listOfIOs.parTraverse(identity)
 
   // hard version
-  def parSequence_v2[F[_] : Traverse, A](wrapperOfIOs: F[IO[A]]): IO[F[A]] =
-    wrapperOfIOs.parTraverse(x => x)
+  def parSequence_v2[F[_]: Traverse, A](wrapperOfIOs: F[IO[A]]): IO[F[A]] =
+    wrapperOfIOs.parTraverse(identity)
+
 
   // existing sequence API
-  val singleIO_v2: IO[List[Int]] = listTraverse.sequence(ios)
+  val singleIO_v2: IO[List[Int]] = Traverse[List].sequence(ios)
 
   // parallel sequencing
   val parallelSingleIO_v2: IO[List[Int]] = parSequence(ios) // from the exercise
-  val parallelSingleIO_v3: IO[List[Int]] = ios.parSequence // extension method from the Parallel syntax package
+  val parallelSingleIO_v3: IO[List[Int]] = ios.parSequence  // extension method from the Parallel syntax package
 
-  override def run =
-    parallelSingleIO_v3.map(_.sum).debug.void
+  var i = 0
+
+  override def run: IO[Unit] =
+    singleIO_v2.map(_.sum).debug.void
+//    parallelSingleIO.map(_.sum).debug.void
+//    traverseFutures_v2(workLoad)(heavyComputation).debug.void
+//    parallelSingleIO_v3.map(_.sum).debug.void
+//    IO(traverseFutures_v2(workLoad)(heavyComputation)).debug
 }
